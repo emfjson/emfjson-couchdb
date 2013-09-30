fd9d8235fe778bfcac86276bf9a6a0313889b082
ghillairet <g.hillairet@gmail.com>
2013-09-30 14:13:03 +0200

    Commit refactored codebase for Couch EMF 0.6.0 - codebase separated in 3 bundles - couchemf.client, rest client for couchdb - couchemf.emfjson, handler for emfjson - couchemf.json, handler for json model

---
+++ bundles/org.eclipselabs.couchemf.emfjson/src/org/eclipselabs/couchemf/emfjson/CouchDBHandler.java
@@ -0,0 +1,75 @@
+package org.eclipselabs.couchemf.emfjson;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.util.Map;
+
+import org.codehaus.jackson.JsonNode;
+import org.codehaus.jackson.JsonProcessingException;
+import org.eclipse.emf.common.util.URI;
+import org.eclipse.emf.ecore.resource.Resource;
+import org.eclipse.emf.ecore.resource.impl.URIHandlerImpl;
+import org.eclipselabs.couchemf.client.CouchClient;
+import org.eclipselabs.emfjson.map.EObjectMapper;
+import org.eclipselabs.emfjson.streams.JsInputStream;
+import org.eclipselabs.emfjson.streams.JsOutputStream;
+
+public class CouchDBHandler extends URIHandlerImpl {
+
+	@Override
+	public boolean canHandle(URI uri) {
+		final CouchClient client = new CouchClient.
+				Builder().
+				url(uri.toString()).
+				build();
+
+		try {			
+			return client.isConnected();
+		} catch (JsonProcessingException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+
+		return false;
+	}
+
+	@Override
+	public InputStream createInputStream(URI uri, Map<?, ?> options) throws IOException {
+		final EObjectMapper mapper = new EObjectMapper();
+		final CouchClient client = new CouchClient.
+				Builder().
+				url(uri.toString()).
+				build();
+
+		return new JsInputStream(uri, options) {
+			@Override
+			public void loadResource(Resource resource) throws IOException {
+				mapper.from(client.get(), resource, options);
+			}
+		};
+	}
+
+	@Override
+	public OutputStream createOutputStream(URI uri, Map<?, ?> options) throws IOException {
+		final CouchClient client = new CouchClient.
+				Builder().
+				url(uri.toString()).
+				build();
+
+		return new JsOutputStream(options) {
+			@Override
+			public void close() throws IOException {
+				JsonNode status = client.put(this.currentRoot);
+				if (status != null && status.has("ok")) {
+					if (status.get("ok").asBoolean()) {
+						String rev = status.get("rev").asText();
+						resource.setURI(resource.getURI().appendQuery("rev="+rev));
+					}
+				}
+			}
+		};
+	}
+
+}
